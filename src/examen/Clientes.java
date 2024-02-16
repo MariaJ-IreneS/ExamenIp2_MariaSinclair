99efa5684bb8275222c12e904c41b65bb5a39fe3
MariaJ-IreneS <misinclair15@unitec.edu>
2024-02-16 14:12:30 -0600

    4clase

---
+++ src/examen/Clientes.java
@@ -0,0 +1,20 @@
+package examen;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Clientes extends Usuario {
+
+    ArrayList<Tramites> tramite;
+
+    public Clientes(String nombre, String apellido, String clave, Date fecha, String sexo, String departamento, String numidentidad, ArrayList tramite) {
+        super(nombre, apellido, clave, fecha, sexo, departamento, numidentidad);
+        this.tramite = tramite;
+    }
+
+    @Override
+    public String toString() {
+        return "Clientes: \n " + "\nTramite: " + tramite + "\n";
+    }
+
+}
